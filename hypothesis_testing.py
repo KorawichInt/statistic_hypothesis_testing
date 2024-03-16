c19cbb0b3bffa3902b0f6761aa94030ae3fc9e11
KorawichInt <korawich.inthamusik@gmail.com>
2024-03-16 22:38:27 +0700

    modify read write csv file part

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-data = pd.read_csv('assets\iris\iris.data', delimiter=' ')
+column_names = ['sepal_length', 'sepal_width',
+                'petal_length', 'petal_width', 'class']
+data = pd.read_csv('assets\iris\iris.data', delimiter=',', names=column_names)
 data.to_csv('csv_repository\iris_data.csv', index=False)
 # df = pd.DataFrame(data)
