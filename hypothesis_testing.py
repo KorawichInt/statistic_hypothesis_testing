2f1ab2915292560fb486aacbfa3254b4c9264e9f
KorawichInt <korawich.inthamusik@gmail.com>
2024-03-16 22:48:59 +0700

    clean code into function main and function read_and_write_iris_data

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
-column_names = ['sepal_length', 'sepal_width',
-                'petal_length', 'petal_width', 'class']
-data = pd.read_csv('assets\iris\iris.data', delimiter=',', names=column_names)
-data.to_csv('csv_repository\iris_data.csv', index=False)
-# df = pd.DataFrame(data)
+
+def read_and_write_iris_data(source, destination):
+    column_names = ['sepal_length', 'sepal_width',
+                    'petal_length', 'petal_width', 'class']
+    data = pd.read_csv(source, delimiter=',', names=column_names)
+    data.to_csv(destination, index=False)
+
+
+if __name__ == "__main__":
+    # read iris.data and write to csv format with columns name
+    source = 'assets\iris\iris.data'
+    destination = 'csv_repository\iris_data.csv'
+    read_and_write_iris_data(source, destination)
