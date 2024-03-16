d0a877571f53a8768260c0b201e645798b658c45
KorawichInt <korawich.inthamusik@gmail.com>
2024-03-17 01:20:59 +0700

    calculate mean proportion of each iris class from 3 datagroup

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -18,28 +18,13 @@ def sampling_data(destination_path, destination_path2):
         frac=1, random_state=42).reset_index(drop=True)
     group_sizes = [80, 38, 32]
     training_set = data_shuffled[:group_sizes[0]]
-    validation_set = data_shuffled[group_sizes[0]                                   :group_sizes[0]+group_sizes[1]]
+    validation_set = data_shuffled[group_sizes[0]:group_sizes[0]+group_sizes[1]]
     testing_set = data_shuffled[group_sizes[0]+group_sizes[1]:]
     training_set.to_csv(destination_path2[0], index=False, header=False)
     validation_set.to_csv(destination_path2[1], index=False, header=False)
     testing_set.to_csv(destination_path2[2], index=False, header=False)
 
 
-# def class_proportion(destination_path, classes):
-#     column_names = ['SepalLength', 'SepalWidth',
-#                     'PetalLength', 'PetalWidth', 'Class']
-#     dataset = pd.read_csv(destination_path, header=None)
-#     dataset.columns = column_names
-#     classes_proportion = []
-#     # classes_proportion.append(dataset['Class']).value_counts(normalize=True)
-#     # classes_proportion.append(dataset['Class']).value_counts(normalize=True)
-#     # classes_proportion.append(dataset['Class']).value_counts(normalize=True)
-#     # for class_name in column_names[:-1]:  # Exclude the last column which is the 'Class'
-#     for class_name in classes:
-#         proportion = dataset[class_name].value_counts(normalize=True)
-#         classes_proportion.append(proportion)
-
-#     return classes_proportion
 def class_proportion(destination_path, classes):
     column_names = ['SepalLength', 'SepalWidth',
                     'PetalLength', 'PetalWidth', 'Class']
@@ -47,8 +32,8 @@ def class_proportion(destination_path, classes):
     dataset.columns = column_names
     class_counts = dataset['Class'].value_counts()
     count_list = class_counts.values.tolist()
-    # proportion_list = [c/sum(count_list) for c in count_list]
-    return count_list
+    proportion_list = [c/sum(count_list) for c in count_list]
+    return proportion_list
 
 
 if __name__ == "__main__":
@@ -81,7 +66,13 @@ if __name__ == "__main__":
     print("\n# Validation set proportion")
     for i in range(len(classes)):
         print(f"{classes[i]} proportion = {validation_set_proportion[i]}")
-    testing_set_validation = class_proportion(destination_path2[2], classes)
+    testing_set_proportion = class_proportion(destination_path2[2], classes)
     print("\n# Training set proportion")
     for i in range(len(classes)):
-        print(f"{classes[i]} proportion = {testing_set_validation[i]}")
+        print(f"{classes[i]} proportion = {testing_set_proportion[i]}")
+
+    print("\nMean Proportion")
+    mean_proportion = [(train + validate + test)/3 for train, validate, test in zip(
+        trainning_set_proportion, validation_set_proportion, testing_set_proportion)]
+    for i in range(len(classes)):
+        print(f"{classes[i]} proportion mean = {mean_proportion[i]}")
