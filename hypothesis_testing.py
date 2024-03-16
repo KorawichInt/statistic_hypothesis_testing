582f6aaad864db4b9ec31233c5f4a414685dbd89
KorawichInt <korawich.inthamusik@gmail.com>
2024-03-17 00:55:37 +0700

    create function class_proportion to count number of each class, each dataset

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -25,6 +25,32 @@ def sampling_data(destination_path, destination_path2):
     testing_set.to_csv(destination_path2[2], index=False, header=False)
 
 
+# def class_proportion(destination_path, classes):
+#     column_names = ['SepalLength', 'SepalWidth',
+#                     'PetalLength', 'PetalWidth', 'Class']
+#     dataset = pd.read_csv(destination_path, header=None)
+#     dataset.columns = column_names
+#     classes_proportion = []
+#     # classes_proportion.append(dataset['Class']).value_counts(normalize=True)
+#     # classes_proportion.append(dataset['Class']).value_counts(normalize=True)
+#     # classes_proportion.append(dataset['Class']).value_counts(normalize=True)
+#     # for class_name in column_names[:-1]:  # Exclude the last column which is the 'Class'
+#     for class_name in classes:
+#         proportion = dataset[class_name].value_counts(normalize=True)
+#         classes_proportion.append(proportion)
+
+#     return classes_proportion
+def class_proportion(destination_path, classes):
+    column_names = ['SepalLength', 'SepalWidth',
+                    'PetalLength', 'PetalWidth', 'Class']
+    dataset = pd.read_csv(destination_path, header=None)
+    dataset.columns = column_names
+    class_counts = dataset['Class'].value_counts()
+    count_list = class_counts.values.tolist()
+    # proportion_list = [c/sum(count_list) for c in count_list]
+    return count_list
+
+
 if __name__ == "__main__":
     # read iris.data and write to csv format with columns name
     source_path = r'assets\iris\iris.data'
@@ -38,11 +64,24 @@ if __name__ == "__main__":
     if not (check_iris_csv):
         print("1")
         read_and_write_data(source_path, destination_path)
-    print("Already 1")
 
     # check if iris_data.csv is exists and training_set is not exists
     check_training_set = check_file_exists(destination_path2[0])
-    if check_iris_csv and (check_training_set):
+    if not (check_training_set):
         print("2")
         sampling_data(destination_path, destination_path2)
-    print("Already 2")
+
+    # find proportion of these classes
+    classes = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+    trainning_set_proportion = class_proportion(destination_path2[0], classes)
+    print("\n# Training set proportion")
+    for i in range(len(classes)):
+        print(f"{classes[i]} proportion = {trainning_set_proportion[i]}")
+    validation_set_proportion = class_proportion(destination_path2[1], classes)
+    print("\n# Validation set proportion")
+    for i in range(len(classes)):
+        print(f"{classes[i]} proportion = {validation_set_proportion[i]}")
+    testing_set_validation = class_proportion(destination_path2[2], classes)
+    print("\n# Training set proportion")
+    for i in range(len(classes)):
+        print(f"{classes[i]} proportion = {testing_set_validation[i]}")
