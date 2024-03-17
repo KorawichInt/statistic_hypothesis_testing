98798548f0ce121d58ca0221522b707c73a475f8
KorawichInt <korawich.inthamusik@gmail.com>
2024-03-17 15:36:36 +0700

    rebuild class count func and add chi square test func

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
-from scipy import stats
+import numpy as np
+from scipy.stats import chi2_contingency
+from scipy.stats import chi2
 
 
 def check_file_exists(file_path):
@@ -18,77 +20,79 @@ def sampling_data(destination_path, destination_path2):
         frac=1, random_state=42).reset_index(drop=True)
     group_sizes = [80, 38, 32]
     training_set = data_shuffled[:group_sizes[0]]
-    validation_set = data_shuffled[group_sizes[0]
-        :group_sizes[0]+group_sizes[1]]
+    validation_set = data_shuffled[group_sizes[0]                                   :group_sizes[0]+group_sizes[1]]
     testing_set = data_shuffled[group_sizes[0]+group_sizes[1]:]
     training_set.to_csv(destination_path2[0], index=False, header=False)
     validation_set.to_csv(destination_path2[1], index=False, header=False)
     testing_set.to_csv(destination_path2[2], index=False, header=False)
 
 
-def class_proportion(destination_path, classes):
+def class_count(destination_path, classes):
     column_names = ['SepalLength', 'SepalWidth',
                     'PetalLength', 'PetalWidth', 'Class']
     dataset = pd.read_csv(destination_path, header=None)
     dataset.columns = column_names
     class_counts = dataset['Class'].value_counts()
-    count_list = class_counts.values.tolist()
-    proportion_list = [c/sum(count_list) for c in count_list]
-    return proportion_list
+    count_dict = class_counts.to_dict()
+    count_dict = {class_name: count_dict[class_name] for class_name in classes}
+    return count_dict
 
 
-def proportion_ztest(mean_proportion, alpha):
-    stat, p_value = stats.proportions_ztest(mean_proportion)
-    print("Z-score:", stat)
-    print("P-value:", p_value)
-    alpha = alpha  # Significance level
-    if p_value < alpha:
-        print("Reject the null hypothesis: Proportions are significantly different.")
-    else:
-        print("Fail to reject the null hypothesis: Proportions are not significantly different.")
+def chi_square_test(observed_df):
+    chi2_stat, p_val, dof, expected_counts = chi2_contingency(observed_df)
+    return chi2_stat, p_val, dof, expected_counts
+
+
+# def decision_and_conclusion():
+#     print("Chi-square statistic:", chi2_stat)
+#     chi_critical = chi2.ppf(1 - alpha, dof)
+#     print("Chi-square critical value:", chi_critical)
+#     # if
+#     print("\nP-value:", p_val)
+#     print("Siginifance Coefficient =", alpha)
 
 
 if __name__ == "__main__":
-    # read iris.data and write to csv format with columns name
+    # define path
     source_path = r'assets\iris\iris.data'
     destination_path = r'csv_repository\iris_data.csv'
     destination_path2 = [r'csv_repository\training_set.csv',
                          r'csv_repository\validation_set.csv',
                          r'csv_repository\testing_set.csv']
 
+    # read iris.data and write to csv format with columns name
     # check if iris_data.csv is not exists
     check_iris_csv = check_file_exists(destination_path)
     if not (check_iris_csv):
-        print("1")
         read_and_write_data(source_path, destination_path)
 
     # check if iris_data.csv is exists and training_set is not exists
     check_training_set = check_file_exists(destination_path2[0])
     if not (check_training_set):
-        print("2")
         sampling_data(destination_path, destination_path2)
 
-    # find proportion of these classes
+    # count number of each classes from 3 group
     classes = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-    trainning_set_proportion = class_proportion(destination_path2[0], classes)
-    print("\n# Training set proportion")
-    for i in range(len(classes)):
-        print(f"{classes[i]} proportion = {trainning_set_proportion[i]}")
-    validation_set_proportion = class_proportion(destination_path2[1], classes)
-    print("\n# Validation set proportion")
-    for i in range(len(classes)):
-        print(f"{classes[i]} proportion = {validation_set_proportion[i]}")
-    testing_set_proportion = class_proportion(destination_path2[2], classes)
-    print("\n# Training set proportion")
-    for i in range(len(classes)):
-        print(f"{classes[i]} proportion = {testing_set_proportion[i]}")
+    training_set_class_count = class_count(destination_path2[0], classes)
+    validation_set_class_count = class_count(destination_path2[1], classes)
+    testing_set_class_count = class_count(destination_path2[2], classes)
+    all_set_dict = {'Traning_set': training_set_class_count,
+                    'Validation_set':  validation_set_class_count,
+                    'Testing_set': testing_set_class_count}
 
-    print("\n# Mean Proportion")
-    mean_proportion = [(train + validate + test)/3 for train, validate, test in zip(
-        trainning_set_proportion, validation_set_proportion, testing_set_proportion)]
-    for i in range(len(classes)):
-        print(f"{classes[i]} proportion mean = {mean_proportion[i]}")
+    # visualize observed freequency with dataframe
+    observed_df = pd.DataFrame.from_dict(all_set_dict)
+    print('\n# Observed Frequency\n', observed_df, '\n')
 
-    # proportion z test
-    alpha = 0.05
-    proportion_ztest(mean_proportion, alpha)
+    # chi-square test
+    chi2_stat, p_val, dof, expected_counts = chi_square_test(
+        observed_df)
+
+    # visualize expected freequency with dataframe
+    p_columns = ['Training_set', 'Validation_set', 'Testing_set']
+    expected_df = pd.DataFrame(
+        expected_counts, index=classes, columns=p_columns)
+    print('# Expected Frequency\n', expected_df, '\n')
+
+    # decision and conclusion
+    # alpha = 0.05
